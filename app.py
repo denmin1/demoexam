c36121f201b83f15b04fafb5abecf235ed11b178
denmin1 <luxsimus666@gmail.com>
2024-11-25 22:24:36 +0300

    Статистика и навигация

--- app.py
+++ app.py
@@ -6,14 +6,15 @@ from fastapi.middleware.cors import CORSMiddleware
 
 class Order(BaseModel):
  number : int
- startdate : datetime.date
+ startDate : datetime.date
  device : str
  problemtype : str
  description : str
  client : str
  status : str
+ endDate: Optional[datetime.date] = None
  master : Optional[str] = "Не назначен"
- comments : Optional[str] = []
+ comments : Optional[list] = []
 
 
 class UpdateOrederDTO(BaseModel):
@@ -27,17 +28,19 @@ repo=[
  
 Order(
  number = 1,
- startdate = "2000-12-01",
+ startDate = "2000-12-01",
  device = "testdev",
  problemtype = "testprobl",
  description = "test",
  client = "ivan",
- status = "testovy"
+ status = "выполнено",
+ endDate = "2000-12-02",
+ problemtype = "uwu"
 ),
 
 Order(
  number = 2,
- startdate = "2000-12-01",
+ startDate = "2000-12-01",
  device = "testdev",
  problemtype = "testprobl",
  description = "test",
@@ -47,7 +50,7 @@ Order(
 
 Order(
  number = 3,
- startdate = "2000-12-01",
+ startDate = "2000-12-01",
  device = "testdev",
  problemtype = "testprobl",
  description = "test",
@@ -95,6 +98,7 @@ def update_order(dto : Annotated[UpdateOrederDTO, Form()]  ):
       message += f"Статус заявки {o.number} изменен\n" 
       if(o.status == "выполнено"):
          message += f"Заявка №{o.number} завершена\n"
+         o.endDate = datetime.datetime.now()
     if dto.description != "":
       o.description = dto.description
     if dto.master != "":
@@ -103,3 +107,29 @@ def update_order(dto : Annotated[UpdateOrederDTO, Form()]  ):
       o.comments.append(dto.comment)
     return o
  return "Не найдено"
+
+def complete_count():
+    return len([o for o in repo if o.status == "выполнено"])
+def get_problem_type_stat():
+    dict = {}
+    for o in repo:
+        if o.problemType in dict.keys():
+            dict[o.problemType] += 1
+        else:
+            dict[o.problemType] = 1
+    return dict
+def get_average_time_to_complete():
+    times = [(
+        datetime.datetime.fromisoformat(o.endDate.isoformat()) -
+        datetime.datetime.fromisoformat(o.startDate.isoformat())).days 
+                 for o in repo 
+                 if o.status == "выполнено"]
+    if complete_count() != 0:
+        return sum(times) / complete_count()
+    return 0
+    
+@app.get("/statistics")
+def get_statistics():
+    return {"complete_count" : complete_count(),
+        "problem_type_stat" : get_problem_type_stat(),
+        "average_time_to_complete" : get_average_time_to_complete()}
